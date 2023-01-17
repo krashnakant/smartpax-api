6983599f7b8c9fb0ea1c081c8d9eeb39e8efbb2c
Rajavel Balasubramanian <bsubramanian.a@gmail.com>
2023-01-17 17:38:23 +0530

    columnstatus

--- src/modules/columnstatuses/columnstatuses.module.ts
+++ src/modules/columnstatuses/columnstatuses.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ColumnstatusesService } from './columnstatuses.service';
 import { ColumnstatusesController } from './columnstatuses.controller';
+import { columnsStatusProviders } from './columnstatuses.provider';
 
 @Module({
   controllers: [ColumnstatusesController],
-  providers: [ColumnstatusesService]
+  providers: [ColumnstatusesService,...columnsStatusProviders]
 })
 export class ColumnstatusesModule {}
